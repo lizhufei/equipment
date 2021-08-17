da26b1783c30d5b5b6f7e0808c89ad1ad572381d
fei <13380320462@163.com>
2021-08-17 15:51:54 +0800

    人员中门表操作

--- src/Equipment.php
+++ src/Equipment.php
@@ -3,6 +3,8 @@
 namespace Hsvisus\Equipment;
 
 
+use Hsvisus\Equipment\Models\Equipment_persons;
+
 class Equipment
 {
     /**
@@ -14,6 +16,41 @@ class Equipment
     {
         return (new Driver())->getEquipmentObject($device_sn);
     }
+
+    /**
+     * 人员设备中间表操作
+     * @param array $device_sns
+     * @param array $person_ids
+     * @param string $operate
+     * @return false
+     */
+    public function intermediary(array $device_sns, array $person_ids, string $operate='save')
+    {
+        $data = [];
+        foreach ($device_sns as $dev){
+            foreach ($person_ids as $per){
+                if ('delete' == $operate){
+                    $data[] = Equipment_persons::where([
+                        ['device_sn', '=', $dev],
+                        ['person_id', '=', $per]
+                    ])->delete();
+
+                }else{
+                    $data[] = [
+                        'device_sn' => $dev,
+                        'person_id' => $per
+                    ];
+                }
+
+            }
+        }
+        if ('save' == $operate){
+            return Equipment_persons::insert($data);
+        }
+        return $data;
+    }
+
+
 
 
 }
